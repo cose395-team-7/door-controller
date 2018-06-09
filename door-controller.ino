cb03bb6aab9315b84ef9917423ee9f49e5d24395
WyldeCat <skan1543@gmail.com>
2018-06-09 21:04:28 +0900

    Write skeleton codes

--- door-controller.ino
+++ door-controller.ino
@@ -1,5 +1,6 @@
 // door-controller.ino
 #include <WiFiEsp.h>
+#include <Servo.h>
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
@@ -7,6 +8,18 @@
 
 #define PIN_WIFI_TX 6
 #define PIN_WIFI_RX 7
+#define PIN_SERVO   9
+
+enum DOOR_STATUS {
+  DOOR_CLOSED,
+  DOOR_OPENED
+};
+
+enum COMMAND {
+  COMMAND_NONE,
+  COMMAND_CLOSE,
+  COMMAND_OPEN
+};
 
 const char *ssid = WIFI_SSID;
 const char *pswd = WIFI_PSWD;
@@ -15,14 +28,46 @@ const char *logicServer = LOGIC_SERVER;
 SoftwareSerial wifiSerial(PIN_WIFI_TX, PIN_WIFI_RX);
 int wifiStatus = WL_IDLE_STATUS;
 
+Servo servo;
+
 void setup()
 {
   Serial.begin(115200);
+  servo.attach(PIN_SERVO);
   setupNetwork();
 }
 
 void loop()
 {
+  switch (fetchCommand()) {
+  case COMMAND_NONE: break;
+  case COMMAND_CLOSE: {
+    onClose(); 
+  }
+  case COMMAND_OPEN: {
+    onOpen();
+  }
+  default: {
+    alertError("Unknown Error");
+  }
+  }
+}
+
+void onClose()
+{
+}
+
+void onOpen()
+{
+}
+
+void alertError(const char* msg)
+{
+}
+
+COMMAND fetchCommand()
+{
+  return COMMAND_OPEN;
 }
 
 void setupNetwork()
